ac63f37935293c3733216008633ff2caa577d2a7
Guilherme Nunes Macario <118843365+usguilherme@users.noreply.github.com>
2025-02-01 08:45:06 -0300

    Create Do_while.java

---
+++ Aula16/Do_while.java
@@ -0,0 +1,16 @@
+package com.loiane.curso_java_basico.aula16;
+
+public class Do_while {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int max = 10;
+		do {
+			i++;
+			System.out.printf("O valor de i é %d\n", i);
+		} while (i < 10);
+		
+		System.out.println("Programa terminou");
+			
+		}
+	}
