6959feebe1f54bcf8c69997b3b0bfe90323a28a2
Guilherme Nunes Macario <118843365+usguilherme@users.noreply.github.com>
2025-02-01 08:45:26 -0300

    Create Estrutura_repeticao.java

---
+++ Aula16/Estrutura_repeticao.java
@@ -0,0 +1,14 @@
+package com.loiane.curso_java_basico.aula16;
+
+public class Estrutura_repeticao {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int max = 10;
+		while (i < max) { //enquanto for verdadeira
+			System.out.println(i);
+			i++;
+				
+		}
+	}
+}
