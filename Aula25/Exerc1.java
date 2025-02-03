b81ffa94e9a8fb01c5d5775891f4eb34a6230502
Guilherme Nunes Macario <118843365+usguilherme@users.noreply.github.com>
2025-02-02 21:20:11 -0300

    Create Exerc1.java

---
+++ Aula25/Exerc1.java
@@ -0,0 +1,13 @@
+package com.loiane.curso_java_basico.aula25;
+
+public class Exerc1 {
+
+	public static void main(String[] args) {
+		Carro carro = new Carro();
+		carro.consumoDeCombustivel = 13.5;
+		carro.quantLitrosNoTanque = 20;
+		
+		carro.exibirDurabilidadeDoCarro(); //Executando	
+		
+	}
+}
