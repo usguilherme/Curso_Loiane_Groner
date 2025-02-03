113ca785c03674c349152466273f0a143b363d56
Guilherme Nunes Macario <118843365+usguilherme@users.noreply.github.com>
2025-02-02 21:08:43 -0300

    Create Lampada.java

---
+++ Aula24_labs/Lampada.java
@@ -0,0 +1,14 @@
+//Escreva uma classe para representar uma lampada que está á venda em um supermercado
+
+package com.loiane.curso_java_basico.aula24_labs;
+
+public class Lampada {
+	String[] tipos;
+	String marca;
+	String tipoDeLuz;
+	String cor;
+	String modelo;
+	double potencia;
+	int garantia;
+
+}
