9e082f007c8aa9c955ecac0f6681e53831ad2cbf
Guilherme Nunes Macario <118843365+usguilherme@users.noreply.github.com>
2025-02-11 09:47:36 -0300

    Create Exerc1.java

---
+++ Aula34/Exerc1.java
@@ -0,0 +1,12 @@
+package exercicio;
+
+public class Exerc1 {
+
+	public static void main(String[] args) {
+		Aluno aluno = new Aluno();
+		aluno.setNome("Guilherme");
+		aluno.setMatricula(null);
+
+	}
+
+}
